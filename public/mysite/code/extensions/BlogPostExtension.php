f988e18c1850e9cf100c920fdae9e02ac3600512
paul headington <paul@pauls-MacBook-Pro.local>
2017-02-17 14:15:29 +1300

    bump build

--- public/mysite/code/extensions/BlogPostExtension.php
+++ public/mysite/code/extensions/BlogPostExtension.php
@@ -10,11 +10,12 @@ class BlogPostExtension extends DataExtension
     private static $many_many = [];
 
     private static $belongs_many_many = [];
-    
+
 
     public function updateCMSFields(FieldList $fields) {
           $fields->insertAfter(new CheckBoxField('Featured', 'Featured'), 'Title');
     }
+
 
    
 }
